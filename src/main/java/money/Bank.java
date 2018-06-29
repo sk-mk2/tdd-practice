4bf183a06766a2e65c3c3bcd9230924d7fca3399
sk-mk2 <ee57042@meiji.ac.jp>
2018-06-29 20:50:22 +0900

    p104

--- src/main/java/money/Bank.java
+++ src/main/java/money/Bank.java
@@ -2,10 +2,14 @@ package money;
 
 class Bank {
     public Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
     }
 
     void addRate(String from, String to, int rate) {
-        
+
+    }
+
+    int rate(String from, String to){
+        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
     }
 }
